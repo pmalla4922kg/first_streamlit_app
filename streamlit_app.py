bee01f0fa75cd680a86502aaedaeda0e61f2c860
MP <114017822+pmalla4922kg@users.noreply.github.com>
2022-09-20 13:51:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,12 @@
 import streamlit
 
 streamlit.title('My Parents New Helathy Dine')
+
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+
+  streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+  streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+  streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+  streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
