41f5a5824b31af008ddb3a757103c0c1efdee63c
MP <114017822+pmalla4922kg@users.noreply.github.com>
2022-09-28 16:09:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,17 +48,17 @@ try:
 #Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  streamlit.error("Please select a fruit to get information")
-  else:
+   if not fruit_choice:
+     streamlit.error("Please select a fruit to get information")
+   else:
     
-#streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+     #streamlit.write('The user entered ', fruit_choice)
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
-# take the json version of the response and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it to the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+     # take the json version of the response and normalize it 
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     # output it to the screen as a table
+     streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error() 
 #Snowflake.connector code..
